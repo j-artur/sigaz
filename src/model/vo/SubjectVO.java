96a5c2828643309c1407c0fffdd01622d040dbae
João Artur <jartur.dev@gmail.com>
2021-09-06 08:17:28 -0300

    Add ID to subjects

--- src/model/vo/SubjectVO.java
+++ src/model/vo/SubjectVO.java
@@ -1,8 +1,20 @@
 package model.vo;
 
 public class SubjectVO {
+	private long id;
 	private String code;
 	private String name;
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		if (id <= 0)
+			System.out.println("ID inválido, deve ser inteiro positivo");
+		else
+			this.id = id;
+	}
 
 	public String getCode() {
 		return this.code;
@@ -21,12 +33,14 @@ public class SubjectVO {
 
 	public void setName(String name) {
 		if (name == null || name.isEmpty())
-			System.out.println("Nome inválido!");
+			System.out.println("Nome vazio");
+		else if (name.length() < 2 || name.length() > 255)
+			System.out.println("Tamanho inválido, nome deve ter entre 2 e 255 caracteres");
 		else
 			this.name = name;
 	}
 
 	public String toString() {
-		return "Código: " + code + "\nNome: " + name;
+		return "ID: " + this.getId() + "\nCódigo: " + this.getCode() + "\nNome: " + this.getName();
 	}
 }
