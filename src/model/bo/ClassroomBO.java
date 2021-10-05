ac693d9cec0c1fa9277b5cfb771d9ce046cbc93c
João Artur <jartur.dev@gmail.com>
2021-10-05 16:56:31 -0300

    ClassroomBO e exceções nos DAO utilizados

---
+++ src/model/bo/ClassroomBO.java
@@ -0,0 +1,73 @@
+package model.bo;
+
+import java.util.List;
+
+import model.dao.ClassroomDAO;
+import model.dao.ClassroomStudentDAO;
+import model.dao.GradeDAO;
+import model.vo.ClassroomVO;
+import model.vo.GradeVO;
+import model.vo.ProfessorVO;
+import model.vo.StudentVO;
+import model.vo.SubjectVO;
+
+public class ClassroomBO implements IClassroomBO {
+	private ClassroomDAO classroomDao = new ClassroomDAO();
+	private ClassroomStudentDAO classroomStudentDao = new ClassroomStudentDAO();
+	private GradeDAO gradeDao = new GradeDAO();
+
+	@Override
+	public void create(ClassroomVO classroom) throws Exception {
+		this.classroomDao.create(classroom);
+	}
+
+	@Override
+	public List<ClassroomVO> findALl() throws Exception {
+		return this.classroomDao.findAll();
+	}
+
+	@Override
+	public List<ClassroomVO> findByProfessor(ProfessorVO professor) throws Exception {
+		return this.classroomDao.findByProfessor(professor);
+	}
+
+	@Override
+	public List<ClassroomVO> findByStudent(StudentVO student) throws Exception {
+		return this.classroomDao.findByStudent(student);
+	}
+
+	@Override
+	public List<ClassroomVO> findBySubject(SubjectVO subject) throws Exception {
+		return this.classroomDao.findBySubject(subject);
+	}
+
+	@Override
+	public void update(ClassroomVO classroom, ClassroomVO data) throws Exception {
+		this.classroomDao.update(classroom, data);
+	}
+
+	@Override
+	public void delete(ClassroomVO classroom) throws Exception {
+		this.classroomStudentDao.delete(classroom);
+		this.classroomDao.delete(classroom);
+	}
+
+	@Override
+	public void add(ClassroomVO classroom, StudentVO student) throws Exception {
+		this.classroomStudentDao.create(classroom, student);
+	}
+
+	@Override
+	public void grade(GradeVO grade) throws Exception {
+		GradeVO persistedGrade = this.gradeDao.findByStudentInClassroom(grade.getStudent(), grade.getClassroom());
+		if (persistedGrade == null)
+			this.gradeDao.create(grade);
+		else
+			this.gradeDao.update(grade, grade);
+	}
+
+	@Override
+	public void remove(ClassroomVO classroom, StudentVO student) throws Exception {
+		this.classroomStudentDao.delete(classroom, student);
+	}
+}
