bf380e6e6a5966a7cf715846dce1ad513bc0f879
João Artur <jartur.dev@gmail.com>
2021-10-05 16:47:39 -0300

    Atualização das interfaces DAO para jogar exceções

--- src/model/dao/IDAO.java
+++ src/model/dao/IDAO.java
@@ -1,13 +1,14 @@
 package model.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IDAO<VO> {
-	public void create(VO vo);
+	public void create(VO vo) throws SQLException;
 
-	public List<VO> findAll();
+	public List<VO> findAll() throws SQLException;
 
-	public void update(VO vo, VO data);
+	public void update(VO vo, VO data) throws SQLException;
 
-	public void delete(VO vo);
+	public void delete(VO vo) throws SQLException;
 }
